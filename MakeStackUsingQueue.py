77d82a85b3dd36e172df00d0a36a1a69b428d8a7
JyotirmayaTembhurne <84905370+JyotirmayaTembhurne@users.noreply.github.com>
2023-09-06 20:01:24 +0530

    Add files via upload
    The goal was to make stack using queue ??? I don't know what the question meant because queue uses First in First out while stacks use Last in First out but regardless..beats 95% of the solutions :D.

--- MakeStackUsingQueue.py
+++ MakeStackUsingQueue.py
@@ -1,59 +1,23 @@
-class MyQueue:
+class MyStack:
     def __init__(self):
-        self.queue = list()
-
-    def length(self):
-        print(len(self.queue))
-
-    def push(self, val):
-        self.queue.append(val)
-        return self.queue
-
-    def pop(self):
-        return self.queue.pop(0)
-
-    def peek(self):
-        return self.queue[0]
-
-    def IsEmpty(self):
-        class MyQueue:
-
-    def __init__(self):
-        self.queue = list()
+        self.stack = list()
 
     def push(self, x: int) -> None:
-        self.queue.append(x) 
+        self.stack.append(x)
 
     def pop(self) -> int:
-        return self.queue.pop(0)
-        
+        return self.stack.pop()
 
-    def peek(self) -> int:
-        return self.queue[0]
-        
+    def top(self) -> int:
+        return self.stack[-1]
 
     def empty(self) -> bool:
-        return self.queue == []
-        
+        return self.stack == []
 
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
 # obj.push(x)
 # param_2 = obj.pop()
-# param_3 = obj.peek()
+# param_3 = obj.top()
 # param_4 = obj.empty()
-
-    def Display(self):
-        for i in self.queue:
-            print(i)
-
-
-q = MyQueue()
-q.push(0)
-q.push(1)
-q.push(2)
-q.push(3)
-q.push(4)
-q.Display()
-print(q.peek())
